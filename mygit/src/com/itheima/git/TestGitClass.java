07e1e9c41a527085c9840ba4eb10d2be4c5dcdc0
wangcheng <1341802315@qq.com>
2018-11-15 20:00:08 +0800

    mygit第一次提交到本地工程

---
+++ mygit/src/com/itheima/git/TestGitClass.java
@@ -0,0 +1,21 @@
+package com.itheima.git;
+
+/**
+ * @program: mygit
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2018-11-15 19:03
+ */
+public class TestGitClass{
+    public static void main(String[] args) {
+        System.out.println("eeeeeeeeeeeeeeeeeeeeee");
+        System.out.println("eeeeeeeeeeeeeeeeeeeeee");
+        System.out.println("eeeeeeeeeeeeeeeeeeeeee");
+        System.out.println("eeeeeeeeeeeeeeeeeeeeee ");
+    }
+
+    public void test1(){
+        int i = 0;
+       i+=3;
+    }
+}
