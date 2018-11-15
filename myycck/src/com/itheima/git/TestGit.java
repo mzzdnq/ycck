d2977350b067b194b82b74470f45a0eeb57e83bb
wangcheng <1341802315@qq.com>
2018-11-15 21:14:24 +0800

    myycck工程提交到本地仓库....

---
+++ myycck/src/com/itheima/git/TestGit.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @program: myycck
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2018-11-15 21:11
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("55555555");
+        System.out.println("55555555");
+        System.out.println("55555555");
+        System.out.println("55555555");
+    }
+}
